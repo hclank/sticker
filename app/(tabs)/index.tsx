6ef7776e8d06083ae944c0c790c60e5a11a8fa8f
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-12-15 14:19:40 +0530

    added button component and imageviewer component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Image } from "expo-image";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
 import { StyleSheet, View } from "react-native";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
@@ -7,7 +8,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -31,9 +36,8 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 420,
-    borderRadius: 18,
+  buttonContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
