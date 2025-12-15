df7c8f6c53522183e2483bd58026af72ab5e813f
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-12-15 13:56:53 +0530

    init commit

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View>
+      <Text>About screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
